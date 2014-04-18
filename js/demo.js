ffbb7214836b9568128a72c2be9a9008960d5630
mittya <mittya@foxmail.com>
2014-04-18 15:09:51 +0800

    Format code

--- js/demo.js
+++ js/demo.js
@@ -1,23 +1,23 @@
 (function() {
 
-    var btn = $('#btn');
-    var taoPlus = $('#taoPlus');
-    var overlay = $('#overlay');
+  var btn = $('#btn');
+  var taoPlus = $('#taoPlus');
+  var overlay = $('#overlay');
 
-    btn.on('click', function() {
-        btn.toggleClass('on');
-        taoPlus.toggleClass('on');
-        overlay.toggleClass('on');
-    });
+  btn.on('click', function() {
+    btn.toggleClass('on');
+    taoPlus.toggleClass('on');
+    overlay.toggleClass('on');
+  });
 
-    overlay.on('click', function() {
-        btn.removeClass('on');
-        taoPlus.removeClass('on');
-        overlay.removeClass('on');
-    });
+  overlay.on('click', function() {
+    btn.removeClass('on');
+    taoPlus.removeClass('on');
+    overlay.removeClass('on');
+  });
 
-    taoPlus.on('click', 'a', function(e) {
-        e.preventDefault();
-    });
+  taoPlus.on('click', 'a', function(e) {
+    e.preventDefault();
+  });
 
 })();
